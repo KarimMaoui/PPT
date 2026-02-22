fb4e7731ef7c8e62f89cdac1927c1ee789dfd0b7
KarimMaoui <karim.maoui@edu.em-lyon.com>
2026-02-23 00:06:45 +0100

    Create App component to render presentation slides

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Presentation } from './components/Presentation';
+import { CoverSlide } from './slides/CoverSlide';
+import { IntroSlide } from './slides/IntroSlide';
+import { AnalyticsSlide } from './slides/AnalyticsSlide';
+import { QuoteSlide } from './slides/QuoteSlide';
+import { OutroSlide } from './slides/OutroSlide';
+
+function App() {
+  return (
+    <Presentation
+      slides={[
+        <CoverSlide key="cover" />,
+        <IntroSlide key="intro" />,
+        <AnalyticsSlide key="analytics" />,
+        <QuoteSlide key="quote" />,
+        <OutroSlide key="outro" />
+      ]}
+    />
+  );
+}
+
+export default App;
