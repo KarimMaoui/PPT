db5d14cc6f3544aa09f5417cc19b74cbf28e2e92
KarimMaoui <karim.maoui@edu.em-lyon.com>
2026-02-23 00:04:19 +0100

    Add Logo component with SVG placeholder

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+// A generic white SVG logo placeholder matching 129x40 specs
+export function Logo() {
+  return (
+    <svg width="129" height="40" viewBox="0 0 129 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-white">
+      <path d="M20 0C8.954 0 0 8.954 0 20C0 31.046 8.954 40 20 40C31.046 40 40 31.046 40 20C40 8.954 31.046 0 20 0ZM20 32C13.373 32 8 26.627 8 20C8 13.373 13.373 8 20 8C26.627 8 32 13.373 32 20C32 26.627 26.627 32 20 32Z" fill="currentColor"/>
+      <rect x="48" y="14" width="81" height="12" rx="4" fill="currentColor" fillOpacity="0.8"/>
+    </svg>
+  );
+}
